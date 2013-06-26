572f6dd16a906584fa4425e5c0512734d46c89a6
Clayton Gulick <claytongulick@gmail.com>
2013-06-25 20:30:39 -0500

    removed check for window
    assigned sentinel flag to track 'this' state instead of checking
    for window or global. This fixes browser/node issues.

--- src/chains.js
+++ src/chains.js
@@ -26,18 +26,16 @@ o_o =  function()
   var args = arguments;
   var fn;
 
-  if(!window) 
-    var window = global; //for running in node
-
   //if this==window this is an initial invocation, create the self context that will be passed to chained calls
   //if this!=window, this is a chained call, and 'self' has been applied as 'this', so get a reference to it
-  var self = (this != window ? this :
+  var self = (this.__o_o__ ? this :
   {
     functions: [],
     current_index: 0,
     execution_map: null,
     error_handler: null,
-    err: null
+    err: null,
+    __o_o__: true //sentinel flag to track current context. Indicates that we're in a chain
   });
 
   /**
